313d22d5dc9e9f737e75e358cf4c035456640d13
Roger Dass <roger.dass@frustuminteractive.com>
2023-08-29 21:50:47 -0700

    CHG: moved iOS/platform defines to a new file so others can use without app framework.

--- include/fi/app/appDefines.h
+++ include/fi/app/appDefines.h
@@ -1,33 +1,7 @@
 #ifndef _APPDEFINES_H
 #define _APPDEFINES_H
 
-#ifdef __APPLE__
-	#include "TargetConditionals.h"
-	#if TARGET_IPHONE_SIMULATOR
-		// iOS Simulator
-		#define IOS
-		#define SIMULATOR
-		#define _IOS
-		#define _SIMULATOR
-	#elif TARGET_OS_IPHONE
-		// iOS device
-		#define _IOS
-		#define IOS
-	#elif TARGET_OS_MAC
-		// Other kinds of Mac OS
-		#if __has_feature(objc_arc)
-			#define ARC
-		#endif
-	#else
-		#error "Unknown Apple platform"
-	#endif
-#else
-	#if LINUX
-	#elif __ANDROID__
-		#define _ANDROID
-	#else
-	#endif /* LINUX */
-#endif /* __APPLE__ */
+#include "fi/platformDefs.h"
 
 #define WND_FULLSCREEN 1
 #define WND_BORDERLESS 2
